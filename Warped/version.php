bf7ba414e6fbcd32746ca2a5df803e47b48838a0
TakeIT2 <i.takeit2@gmail.com>
2010-06-22 20:31:36 +0000

    code formatting, version update to 2.1

--- Warped/version.php
+++ Warped/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Warped';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('warped', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('warped template', $domain);
-$themeversion['author'] 	= 'Mark West, James Koster';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.sixshootermedia.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Warped';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('warped', $domain);
+$themeversion['description'] = __('warped template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, James Koster';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.sixshootermedia.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
