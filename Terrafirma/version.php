32c22491c5793678a27c3803afca239e824856e1
TakeIT2 <i.takeit2@gmail.com>
2010-06-23 19:37:08 +0000

    code formatting, version update to 2.1

--- Terrafirma/version.php
+++ Terrafirma/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Terrafirma';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('terrafirma', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('terrafirma OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, nodethirtythree';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.nodethirtythree.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.TXT';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Terrafirma';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('terrafirma', $domain);
+$themeversion['description'] = __('terrafirma OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, nodethirtythree';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.nodethirtythree.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.TXT';
+$themeversion['xhtml'] = true;
