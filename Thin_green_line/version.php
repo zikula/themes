0605a61fa24a692de4ee3f9434bcf511c32b2f4a
TakeIT2 <i.takeit2@gmail.com>
2010-06-23 12:49:12 +0000

    code formatting, version update to 2.1

--- Thin_green_line/version.php
+++ Thin_green_line/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Thin_green_line';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Thin Green Line', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Thin green line template', $domain);
-$themeversion['author'] 	= 'Mark West, James Koster';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.sixshootermedia.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= 'docs/LICENSE.txt';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Thin_green_line';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('Thin Green Line', $domain);
+$themeversion['description'] = __('Thin green line template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, James Koster';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.sixshootermedia.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = 'docs/LICENSE.txt';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
