77454e7327e444c7f4598ca26deb75847fd5f5fa
TakeIT2 <i.takeit2@gmail.com>
2010-06-23 21:50:01 +0000

    code formatting, version update to 2.1

--- Techmania/version.php
+++ Techmania/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Techmania';
+$themeversion['name'] = 'Techmania';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Techmania', $domain);
-$themeversion['description'] 	= __('Techmania template', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['author'] 	= 'Mark West, Erwin Aligam';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.styleshout.com';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Techmania', $domain);
+$themeversion['description'] = __('Techmania template', $domain);
+$themeversion['author'] = 'Mark West, Erwin Aligam';
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.0';
+$themeversion['official']   = '0';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.styleshout.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
