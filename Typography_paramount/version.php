bb14e148f44f788aa10d0a366166722406d6e602
TakeIT2 <i.takeit2@gmail.com>
2010-06-23 12:21:12 +0000

    code formatting, version update to 2.1

--- Typography_paramount/version.php
+++ Typography_paramount/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Typography_paramount';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Typography_Paramount', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Typography paramount template', $domain);
-$themeversion['author'] 	= 'Mark West, James Koster';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.sixshootermedia.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Typography_paramount';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('Typography_Paramount', $domain);
+$themeversion['description'] = __('Typography paramount template', $domain)
+;$themeversion['regid'] = '0';
+$themeversion['version'] = '2.1';
+$themeversion['official'] = '0';
+$themeversion['author'] = 'Mark West, James Koster';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.sixshootermedia.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
