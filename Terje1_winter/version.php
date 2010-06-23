b1a24ac8fdadeadb962b3d76a5a68af415a5d3a3
TakeIT2 <i.takeit2@gmail.com>
2010-06-23 21:41:43 +0000

    code formatting, version update to 2.1

--- Terje1_winter/version.php
+++ Terje1_winter/version.php
@@ -1,19 +1,18 @@
 <?php
-$themeversion['name'] 		= 'Terge1_winter';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('terge1_winter', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('terge1 winter OSWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Asphaug.info';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://asphaug.info';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= 'docs/';
-$themeversion['help'] 		= 'docs/';
-$themeversion['changelog'] 	= 'docs/';
-$themeversion['license'] 	= 'docs/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Terge1_winter';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('terge1_winter', $domain);
+$themeversion['description'] = __('terge1 winter OSWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Asphaug.info';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://asphaug.info';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = 'docs/';
+$themeversion['help'] = 'docs/';
+$themeversion['changelog'] = 'docs/';
+$themeversion['license'] = 'docs/';
+$themeversion['xhtml'] = true;
