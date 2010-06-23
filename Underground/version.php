5f208911ca46f7a8e99edaa60a4145c0e8acce17
TakeIT2 <i.takeit2@gmail.com>
2010-06-23 19:30:54 +0000

    code formatting, version update to 2.1

--- Underground/version.php
+++ Underground/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Underground';
+$themeversion['name'] = 'Underground';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Underground', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Underground template', $domain);
-$themeversion['author'] 	= 'Mark West, Erwin Aligam';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.styleshout.com';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Underground', $domain);
+$themeversion['description'] = __('Underground template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Erwin Aligam';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.styleshout.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
