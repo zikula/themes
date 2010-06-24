30f7b5c90ca16fdb734716e90deb90f1b5e85362
TakeIT2 <i.takeit2@gmail.com>
2010-06-24 22:29:17 +0000

    code formatting, version update to 2.1

--- Nature/version.php
+++ Nature/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Nature';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Nature', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Nature OSWD template', $domain);
-$themeversion['author'] 	= 'Mark West, kty';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.studio-plume.org';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Nature';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('Nature', $domain);
+$themeversion['description'] = __('Nature OSWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, kty';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.studio-plume.org';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
