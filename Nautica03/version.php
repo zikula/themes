531e9f558dfc36cabe3ce559f76b0c3daae5c1e3
TakeIT2 <i.takeit2@gmail.com>
2010-06-24 22:12:49 +0000

    code formatting, version update to 2.1

--- Nautica03/version.php
+++ Nautica03/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Nautica03';
+$themeversion['name'] = 'Nautica03';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Nautica03', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Nautica03 OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Nautical';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.opensourcetemplates.org/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Nautica03', $domain);
+$themeversion['description'] = __('Nautica03 OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Nautical';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.opensourcetemplates.org/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
