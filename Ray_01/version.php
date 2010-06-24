5094aee5a633e84d2a967a8ebc05808a29d1d5e2
TakeIT2 <i.takeit2@gmail.com>
2010-06-24 19:59:51 +0000

    code formatting, version update to 2.1

--- Ray_01/version.php
+++ Ray_01/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Ray_01';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Ray_01', $domain);
-$themeversion['version'] 	= '2.0';
-$themeversion['regid'] 		= '0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Ray_01 OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Raymond Gerrard';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://raygerrard.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Ray_01';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('Ray_01', $domain);
+$themeversion['description'] = __('Ray_01 OWD template', $domain);
+$themeversion['version']    = '2.1';
+$themeversion['regid']      = '0';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Raymond Gerrard';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://raygerrard.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
