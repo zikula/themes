384a2781328d70722c69f6c49849fc07e53cc81f
TakeIT2 <i.takeit2@gmail.com>
2010-06-24 22:20:33 +0000

    code formatting, version update to 2.1

--- Nautica02/version.php
+++ Nautica02/version.php
@@ -1,19 +1,18 @@
 <?php
-$themeversion['name'] 		= 'Nautica02';
+$themeversion['name'] = 'Nautica02';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Nautica02', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Nautica02 OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Aran Down';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.studio7designs.com';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= 'docs/readme.txt';
-$themeversion['help'] 		= 'docs/readme.txt';
-$themeversion['changelog'] 	= 'docs/readme.txt';
-$themeversion['license'] 	= 'docs/readme.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Nautica02', $domain);
+$themeversion['description'] = __('Nautica02 OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Aran Down';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.studio7designs.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = 'docs/readme.txt';
+$themeversion['help'] = 'docs/readme.txt';
+$themeversion['changelog'] = 'docs/readme.txt';
+$themeversion['license'] = 'docs/readme.txt';
+$themeversion['xhtml'] = true;
