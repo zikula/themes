6676dc3e093b33aae3ea5c3560feb9cc22b81535
TakeIT2 <i.takeit2@gmail.com>
2010-06-24 20:46:43 +0000

    code formatting, version update to 2.1

--- Plain/version.php
+++ Plain/version.php
@@ -1,19 +1,18 @@
 <?php
-$themeversion['name'] 		= 'Plain';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('plain', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '1.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('plain OSWD template', $domain);
-$themeversion['author'] 	= 'Mark West, James Koster';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.jameskoster.co.uk/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Plain';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('plain', $domain);
+$themeversion['description'] = __('plain OSWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, James Koster';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.jameskoster.co.uk/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
