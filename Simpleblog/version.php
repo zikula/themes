567827c65fde4864a9a6d73df2b6e3e4739c0487
TakeIT2 <i.takeit2@gmail.com>
2010-06-24 19:28:29 +0000

    code formatting, version update to 2.1

--- Simpleblog/version.php
+++ Simpleblog/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Simpleblog';
+$themeversion['name'] = 'Simpleblog';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Simple_Blog', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Simple Blog template', $domain);
-$themeversion['author'] 	= 'Mark West, Erwin Aligam';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.styleshout.com';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Simple_Blog', $domain);
+$themeversion['description'] = __('Simple Blog template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Erwin Aligam';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.styleshout.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
