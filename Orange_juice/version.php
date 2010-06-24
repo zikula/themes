a47803e5f311b3d35a1ef9d10357d3780e1e933c
TakeIT2 <i.takeit2@gmail.com>
2010-06-24 21:19:10 +0000

    code formatting, version update to 2.1

--- Orange_juice/version.php
+++ Orange_juice/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Orange_juice';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Orange Juice', $domain);
-$themeversion['version'] 	= '2.0';
-$themeversion['description'] 	= __('Orange Juice OWD template', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['official'] 	= '0';
-$themeversion['author'] 	= 'Mark West, Denise Mitchinson';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.mitchinson.net';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/licence.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Orange_juice';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('Orange Juice', $domain);
+$themeversion['description'] = __('Orange Juice OWD template', $domain);
+$themeversion['version']    = '2.1';
+$themeversion['regid'] = '0';
+$themeversion['official'] = '0';
+$themeversion['author'] = 'Mark West, Denise Mitchinson';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.mitchinson.net';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/licence.txt';
+$themeversion['xhtml'] = true;
