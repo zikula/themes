8cf7a587b4523f2d282fb33903bb56b09d6f52ae
TakeIT2 <i.takeit2@gmail.com>
2010-06-27 20:49:10 +0000

    code formatting, version update to 2.1

--- Naturalist/version.php
+++ Naturalist/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Naturalist';
-$domain 			= ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Naturalist', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Naturalist OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Darren Hester';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.designsbydarren.com';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.TXT';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['name'] = 'Naturalist';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('Naturalist', $domain);
+$themeversion['description'] = __('Naturalist OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Darren Hester';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.designsbydarren.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.TXT';
+$themeversion['xhtml'] = true;
