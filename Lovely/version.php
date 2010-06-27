061df9f7cda7b3455c87b7bc5a49217e0111afe6
TakeIT2 <i.takeit2@gmail.com>
2010-06-27 22:41:28 +0000

    code formatting, version update to 2.1

--- Lovely/version.php
+++ Lovely/version.php
@@ -1,19 +1,18 @@
 <?php
-$themeversion['name'] 		= 'Lovely';
+$themeversion['name'] = 'Lovely';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Lovely', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Lovely OSWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Kumiko';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.kumi.co.nr/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= 'docs/readme.txt';
-$themeversion['help'] 		= 'docs/readme.txt';
-$themeversion['changelog'] 	= 'docs/readme.txt';
-$themeversion['license'] 	= 'docs/readme.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Lovely', $domain);
+$themeversion['description'] = __('Lovely OSWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Kumiko';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.kumi.co.nr/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = 'docs/readme.txt';
+$themeversion['help'] = 'docs/readme.txt';
+$themeversion['changelog'] = 'docs/readme.txt';
+$themeversion['license'] = 'docs/readme.txt';
+$themeversion['xhtml'] = true;
