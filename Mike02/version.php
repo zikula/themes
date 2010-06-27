caecc72afd99f890d444a3f704fde170d6cff564
TakeIT2 <i.takeit2@gmail.com>
2010-06-27 21:47:07 +0000

    code formatting, version update to 2.1

--- Mike02/version.php
+++ Mike02/version.php
@@ -1,19 +1,18 @@
 <?php
-$themeversion['name'] 		= 'Mike02';
+$themeversion['name'] = 'Mike02';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('mike02', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('mike02 OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Mike Yarmish';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.mikeyarmish.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('mike02', $domain);
+$themeversion['description'] = __('mike02 OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Mike Yarmish';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.mikeyarmish.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
