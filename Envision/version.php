99b23664676fc025bf770c5f1bf19631557f8baa
TakeIT2 <i.takeit2@gmail.com>
2010-06-28 21:35:23 +0000

    code formatting, version update to 2.1

--- Envision/version.php
+++ Envision/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Envision';
+$themeversion['name'] = 'Envision';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= _('Envision', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '1.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= _('Envision template', $domain);
-$themeversion['author'] 	= 'Mark West, Erwin Aligam';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.styleshout.com';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = _('Envision', $domain);
+$themeversion['description'] = _('Envision template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Erwin Aligam';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.styleshout.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
