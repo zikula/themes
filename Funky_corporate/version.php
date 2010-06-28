8c064ba306c10c383c6ea9be1259229fbdbfe05f
TakeIT2 <i.takeit2@gmail.com>
2010-06-28 19:27:23 +0000

    code formatting, version update to 2.1

--- Funky_corporate/version.php
+++ Funky_corporate/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Funky_corporate';
+$themeversion['name'] = 'Funky_corporate';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Funky Corporate', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Funky Corporate OWD Template', $domain);
-$themeversion['author'] 	= 'Mark West, Empire Elements';
-$themeversion['contact'] 	= 'http/www.markwest.me.uk, http/www.empire-elements.co.uk';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Funky Corporate', $domain);
+$themeversion['description'] = __('Funky Corporate OWD Template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Empire Elements';
+$themeversion['contact'] = 'http/www.markwest.me.uk, http/www.empire-elements.co.uk';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
