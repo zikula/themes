16b5ca7f871504dc524a53a744da766deb3e2c32
TakeIT2 <i.takeit2@gmail.com>
2010-06-28 20:24:09 +0000

    code formatting, version update to 2.1

--- Frametheme/version.php
+++ Frametheme/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Frametheme';
+$themeversion['name'] = 'Frametheme';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Frame Theme', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Frame Theme', $domain);
-$themeversion['author'] 	= 'Mark West, Kai Ackermann';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.northern-web-coders.de/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Frame Theme', $domain);
+$themeversion['description'] = __('Frame Theme', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Kai Ackermann';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.northern-web-coders.de/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
