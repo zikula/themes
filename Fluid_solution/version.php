c50d747b6b6ccff401932e01c804c88dc049076d
TakeIT2 <i.takeit2@gmail.com>
2010-06-28 20:36:58 +0000

    code formatting, version update to 2.1

--- Fluid_solution/version.php
+++ Fluid_solution/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Fluid_solution';
+$themeversion['name'] = 'Fluid_solution';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Fluid Solution', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Fluid Solution OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Arcsin';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.arcsin.se';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Fluid Solution', $domain);
+$themeversion['description'] = __('Fluid Solution OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Arcsin';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.arcsin.se';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.txt';
+$themeversion['xhtml'] = true;
