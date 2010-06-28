311c31d6bde85a0b2204ee12466edcf6b1a99be4
TakeIT2 <i.takeit2@gmail.com>
2010-06-28 18:30:56 +0000

    code formatting, version update to 2.1

--- Glossyblue/version.php
+++ Glossyblue/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Glossyblue';
+$themeversion['name'] = 'Glossyblue';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('GlossyBlue', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('GlossyBlue template', $domain);
-$themeversion['author'] 	= 'Mark West, Nick La';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.ndesign-studio.com/resources/wp-themes/glossyblue/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('GlossyBlue', $domain);
+$themeversion['description'] = __('GlossyBlue template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Nick La';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.ndesign-studio.com/resources/wp-themes/glossyblue/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.txt';
+$themeversion['xhtml'] = true;
