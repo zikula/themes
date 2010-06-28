faecb47cebeffd3b65f333de472048c469ef4826
TakeIT2 <i.takeit2@gmail.com>
2010-06-28 16:48:14 +0000

    code formatting, version update to 2.1

--- Invention/version.php
+++ Invention/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Invention';
+$themeversion['name'] = 'Invention';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Invention', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '1.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Invention 2 OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Sompura';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.openwebdesign.org/userinfo.phtml?user=sompura';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.TXT';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Invention', $domain);
+$themeversion['description'] = __('Invention 2 OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Sompura';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.openwebdesign.org/userinfo.phtml?user=sompura';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.TXT';
+$themeversion['xhtml'] = true;
