781e67ca26deaabdaf443a58d0c83475e64c3c53
TakeIT2 <i.takeit2@gmail.com>
2010-06-28 08:14:29 +0000

    code formatting, version update to 2.1

--- Kubrick/version.php
+++ Kubrick/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Kubrick';
+$themeversion['name'] = 'Kubrick';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('kubrick', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Kubrick template by Michael Heilemann', $domain);
-$themeversion['author'] 	= 'Mark West, Michael Heilemann';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://binarybonsai.com/kubrick/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('kubrick', $domain);
+$themeversion['description'] = __('Kubrick template by Michael Heilemann', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Michael Heilemann';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://binarybonsai.com/kubrick/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.txt';
+$themeversion['xhtml'] = true;
