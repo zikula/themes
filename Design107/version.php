dc2e7f9d9dd8eefa38d621366914d0605c7421c0
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 03:35:52 +0000

    code formatting, version update to 2.1

--- Design107/version.php
+++ Design107/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Design107';
+$themeversion['name'] = 'Design107';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('design107', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '1.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('design107 OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, dcarter';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.dcarter.co.uk/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.TXT';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('design107', $domain);
+$themeversion['description'] = __('design107 OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, dcarter';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.dcarter.co.uk/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.TXT';
+$themeversion['xhtml'] = true;
