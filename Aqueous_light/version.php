9e4f76fe982355a3527e3257e4b4f76fd348a7b5
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 14:32:18 +0000

    code formatting, version update to 2.1

--- Aqueous_light/version.php
+++ Aqueous_light/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Aqueous_light';
+$themeversion['name'] = 'Aqueous_light';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Aqueous Light', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Aqueous Light template', $domain);
-$themeversion['author'] 	= 'Mark West, James Koster';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.sixshootermedia.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Aqueous Light', $domain);
+$themeversion['description'] = __('Aqueous Light template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, James Koster';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.sixshootermedia.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
