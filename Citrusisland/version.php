e1af46c082f18d7494076ca7172eaee53568b431
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 07:02:22 +0000

    code formatting, version update to 2.1

--- Citrusisland/version.php
+++ Citrusisland/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Citrusisland';
+$themeversion['name'] = 'Citrusisland';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Citrus Island', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Citrus Island template', $domain);
-$themeversion['author'] 	= 'Mark West, Erwin Aligam';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.styleshout.com';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'http://creativecommons.org/licenses/by/2.5/';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Citrus Island', $domain);
+$themeversion['description'] = __('Citrus Island template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Erwin Aligam';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.styleshout.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'http://creativecommons.org/licenses/by/2.5/';
+$themeversion['xhtml'] = true;
