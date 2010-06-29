fc0104a3f960d3e3b1e126f87737768bb6a8dd93
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 14:23:40 +0000

    code formatting, version update to 2.1

--- Atomo/version.php
+++ Atomo/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Atomo';
+$themeversion['name'] = 'Atomo';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Atomo', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Atomo (formerly atomohost) was derived from the atomohost template', $domain);
-$themeversion['author'] 	= 'Mark West, Free CSS Templates';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.freecsstemplates.org';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/license.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Atomo', $domain);
+$themeversion['description'] = __('Atomo (formerly atomohost) was derived from the atomohost template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Free CSS Templates';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.freecsstemplates.org';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/license.txt';
+$themeversion['xhtml'] = true;
