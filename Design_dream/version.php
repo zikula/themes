2494f5ed755d0fd5485d3b982d09f4e5a5d18bb6
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 03:25:36 +0000

    code formatting, version update to 2.1

--- Design_dream/version.php
+++ Design_dream/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Design_dream';
+$themeversion['name'] = 'Design_dream';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Design Dream', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Design Dream OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, J David Macor';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.jdavidmacor.com';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Design Dream', $domain);
+$themeversion['description'] = __('Design Dream OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, J David Macor';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.jdavidmacor.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
