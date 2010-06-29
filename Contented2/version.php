54e7b6d0a679823092805747965d3c8fe32c7a33
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 06:11:44 +0000

    code formatting, version update to 2.1

--- Contented2/version.php
+++ Contented2/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Contented2';
+$themeversion['name'] = 'Contented2';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Contented2', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Contented2 OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Carlo Forghieri';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.drugo.biz/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Contented2', $domain);
+$themeversion['description'] = __('Contented2 OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Carlo Forghieri';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.drugo.biz/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
