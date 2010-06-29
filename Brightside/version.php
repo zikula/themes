ba1f81646c4a41df8b499f870a396ae8a7c60d92
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 07:38:19 +0000

    code formatting, version update to 2.1

--- Brightside/version.php
+++ Brightside/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Brightside';
+$themeversion['name'] = 'Brightside';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Brightside', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Brightside Open Designs template', $domain);
-$themeversion['author'] 	= 'Mark West, Erwin Aligam';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.styleshout.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/license.html';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Brightside', $domain);
+$themeversion['description'] = __('Brightside Open Designs template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Erwin Aligam';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.styleshout.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/license.html';
+$themeversion['xhtml'] = true;
