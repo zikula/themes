0ddfbf7aab875b235c87ad445509b580dae8fd38
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 05:23:09 +0000

    code formatting, version update to 2.1

--- Decayed/version.php
+++ Decayed/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Decayed';
+$themeversion['name'] = 'Decayed';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Decayed', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Decayed template', $domain);
-$themeversion['author'] 	= 'Mark West, Arcsin';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://arcsin.se';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/license.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Decayed', $domain);
+$themeversion['description'] = __('Decayed template', $domain);
+$themeversion['regid'] = '0';
+$themeversion['version'] = '2.1';
+$themeversion['official'] = '0';
+$themeversion['author'] = 'Mark West, Arcsin';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://arcsin.se';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/license.txt';
+$themeversion['xhtml'] = true;
