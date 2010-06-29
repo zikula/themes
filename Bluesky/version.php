b7f5ba2596fd630aa98963d552e842a0c6e6a748
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 14:01:59 +0000

    code formatting, version update to 2.1

--- Bluesky/version.php
+++ Bluesky/version.php
@@ -1,19 +1,18 @@
 <?php
-$themeversion['name'] 		= 'Bluesky';
+$themeversion['name'] = 'Bluesky';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Blue Sky', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Blue Sky OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, Jonas John';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.jonasjohn.de/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= '';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Blue Sky', $domain);
+$themeversion['description'] = __('Blue Sky OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Jonas John';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.jonasjohn.de/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = '';
+$themeversion['xhtml'] = true;
