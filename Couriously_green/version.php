0806645569edd394e57a2f8010309c260d80fc6e
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 05:31:05 +0000

    code formatting, version update to 2.1

--- Couriously_green/version.php
+++ Couriously_green/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Curiously_green';
+$themeversion['name'] = 'Curiously_green';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Curiously Green', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('curiously_green OWD template', $domain);
-$themeversion['author'] 	= 'Mark West, node33';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.nodethirtythree.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.txt';
-$themeversion['xhtml'] 		= true;
-?>
+$themeversion['displayname'] = __('Curiously Green', $domain);
+$themeversion['description'] = __('curiously_green OWD template', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, node33';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.nodethirtythree.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.txt';
+$themeversion['xhtml'] = true;
