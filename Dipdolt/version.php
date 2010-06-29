995ae3ed0dd2bbd5c65d98ca18dd07a8a050bb30
TakeIT2 <i.takeit2@gmail.com>
2010-06-29 02:31:37 +0000

    code formatting, version update to 2.1

--- Dipdolt/version.php
+++ Dipdolt/version.php
@@ -1,19 +1,18 @@
 <?php 
-$themeversion['name'] 		= 'Dipdolt';
+$themeversion['name'] = 'Dipdolt';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('Dipdolt', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '1.0';
-$themeversion['official'] 	= '0';
-$themeversion['description'] 	= __('Dipdolt OWD templates', $domain);
-$themeversion['author'] 	= 'Mark West, Darjan Panic, Brian Green';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.darjanpanic.com/, http://www.briangreens.com/';
-$themeversion['admin'] 		= 0;
-$themeversion['user'] 		= 1;
-$themeversion['system'] 	= 0;
-$themeversion['credits'] 	= '';
-$themeversion['help'] 		= '';
-$themeversion['changelog'] 	= '';
-$themeversion['license'] 	= 'docs/LICENSE.TXT';
-$themeversion['xhtml'] 		= True;
-?>
+$themeversion['displayname'] = __('Dipdolt', $domain);
+$themeversion['description'] = __('Dipdolt OWD templates', $domain);
+$themeversion['regid']      = '0';
+$themeversion['version']    = '2.1';
+$themeversion['official']   = '0';
+$themeversion['author'] = 'Mark West, Darjan Panic, Brian Green';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.darjanpanic.com/, http://www.briangreens.com/';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/LICENSE.TXT';
+$themeversion['xhtml'] = True;
