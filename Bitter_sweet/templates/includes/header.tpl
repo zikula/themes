2f4c818b8cb0ef00b6e37a35a4092d3ce161a48d
TakeIT2 <i.takeit2@gmail.com>
2010-06-30 16:30:00 +0000

    initial add of theme Bitter_sweet

---
+++ Bitter_sweet/templates/includes/header.tpl
@@ -0,0 +1,5 @@
+<div id="top">
+    <a href="{homepage}"><span>{sitename}</span></a>
+</div>
+<div class="header"></div>
+
---
+++ Bitter_sweet/version.php
@@ -0,0 +1,18 @@
+<?php
+$themeversion['name'] 		= 'Bitter_sweet';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] 	= __('Bitter_sweet', $domain);
+$themeversion['description'] 	= __('Bitter Sweet OWD template', $domain);
+$themeversion['regid'] 		= '0';
+$themeversion['version'] 	= '2.0';
+$themeversion['official'] 	= '0';
+$themeversion['author'] 	= 'Mark West, Arcsin';
+$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.arcsin.se';
+$themeversion['admin'] 		= 0;
+$themeversion['user'] 		= 1;
+$themeversion['system'] 	= 0;
+$themeversion['changelog'] 	= 'docs/';
+$themeversion['credits'] 	= 'docs/';
+$themeversion['help'] 		= 'docs/';
+$themeversion['license'] 	= 'docs/';
+$themeversion['xhtml'] 		= true;
