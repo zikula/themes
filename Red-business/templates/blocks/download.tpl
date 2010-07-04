bb86c892a321015bcf8a9cefb4ce21ad434bcd3e
TakeIT2 <i.takeit2@gmail.com>
2010-07-04 17:33:46 +0000

    initial add of theme Red-business

---
+++ Red-business/templates/blocks/download.tpl
@@ -0,0 +1,6 @@
+<div id="dl">
+    {if $title neq ''}
+    <h4>{$title}</h4>
+    {/if}
+    {$content}
+</div>
---
+++ Red-business/version.php
@@ -0,0 +1,18 @@
+<?php 
+$themeversion['name'] = 'Red-business';
+$domain = ZLanguage::getThemeDomain($themeversion['name']);
+$themeversion['displayname'] = __('Red-Business', $domain);
+$themeversion['description'] = __('Red-Business template', $domain);
+$themeversion['regid'] = '0';
+$themeversion['version'] = '1.0';
+$themeversion['official'] = '0';
+$themeversion['author'] = 'Mark West, David Herreman';
+$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.free-css-templates.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/license.txt';
+$themeversion['xhtml'] = true;
