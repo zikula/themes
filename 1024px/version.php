582ca84ce3977a34c29f092a46f2c464c277344b
TakeIT2 <i.takeit2@gmail.com>
2010-07-15 16:09:59 +0000

    code formatting

--- 1024px/version.php
+++ 1024px/version.php
@@ -1,16 +1,16 @@
 <?php
-$themeversion['name'] 		= '1024px';
+$themeversion['name'] = '1024px';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] 	= __('1024px', $domain);
-$themeversion['description'] 	= __('1024px OWSD Template', $domain);
-$themeversion['regid'] 		= '0';
-$themeversion['version'] 	= '2.1';
-$themeversion['official'] 	= '0';
-$themeversion['author'] 	= 'Mark West, Andreas Viklund';
-$themeversion['contact'] 	= 'http://www.markwest.me.uk, http://www.andreasviklund.com';
-$themeversion['admin']		 = 0;
-$themeversion['user']		 = 1;
-$themeversion['system']		 = 0;
+$themeversion['displayname'] = __('1024px', $domain);
+$themeversion['description'] = __('1024px OWSD Template', $domain);
+$themeversion['regid'] = '0';
+$themeversion['version'] = '2.1';
+$themeversion['official'] = '0';
+$themeversion['author'] = 'TakeIT2, Mark West, Andreas Viklund';
+$themeversion['contact'] = 'http://code.zikula.org/themes/, http://www.markwest.me.uk, http://www.andreasviklund.com';
+$themeversion['admin'] = 0;
+$themeversion['user'] = 1;
+$themeversion['system'] = 0;
 $themeversion['credits'] = 'docs/readme.txt';
 $themeversion['help'] = 'docs/readme.txt';
 $themeversion['changelog'] = 'docs/readme.txt';
