5df402d95ac05febf332ea7f25fdc2656bd36855
TakeIT2 <i.takeit2@gmail.com>
2010-07-18 00:57:48 +0000

    code formatting

--- Mike02/version.php
+++ Mike02/version.php
@@ -1,13 +1,13 @@
 <?php
 $themeversion['name'] = 'Mike02';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] = __('mike02', $domain);
-$themeversion['description'] = __('mike02 OWD template', $domain);
+$themeversion['displayname'] = __('Mike02', $domain);
+$themeversion['description'] = __('Mike02 OWD template', $domain);
 $themeversion['regid'] = '0';
 $themeversion['version'] = '2.1';
 $themeversion['official'] = '0';
-$themeversion['author'] = 'Mark West, Mike Yarmish';
-$themeversion['contact'] = 'http://www.markwest.me.uk, http://www.mikeyarmish.com/';
+$themeversion['author'] = 'TakeIT2, Mark West, Mike Yarmish';
+$themeversion['contact'] = 'http://code.zikula.org/themes/, http://www.markwest.me.uk, http://www.mikeyarmish.com/';
 $themeversion['admin'] = 0;
 $themeversion['user'] = 1;
 $themeversion['system'] = 0;
