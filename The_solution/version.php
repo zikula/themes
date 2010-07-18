5b9353ef21c0c0fd8868274470880b0c264f03c8
TakeIT2 <i.takeit2@gmail.com>
2010-07-18 18:29:11 +0000

    code formatting

--- The_solution/version.php
+++ The_solution/version.php
@@ -1,18 +1,18 @@
 <?php
 $themeversion['name'] = 'The_solution';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] = __('The_solution'$domain);
-$themeversion['description'] = __('The_solution OWD template'$domain);
+$themeversion['displayname'] = __('The_Solution', $domain);
+$themeversion['description'] = __('The Solution OWD template', $domain);
 $themeversion['regid'] = '0';
 $themeversion['version'] = '2.1';
 $themeversion['official'] = '0';
-$themeversion['author'] = 'Mark West, Jasper van der Meer';
-$themeversion['contact'] = 'http://www.markwest.me.uk, http://jmstudio.nl/';
+$themeversion['author'] = 'TakeIT2, Mark West, Jasper van der Meer';
+$themeversion['contact'] = 'http://code.zikula.org/themes/, http://www.markwest.me.uk, http://jmstudio.nl/';
 $themeversion['admin'] = 0;
 $themeversion['user'] = 1;
 $themeversion['system'] = 0;
+$themeversion['changelog'] = 'docs/';
 $themeversion['credits'] = 'docs/';
 $themeversion['help'] = 'docs/';
-$themeversion['changelog'] = 'docs/';
 $themeversion['license'] = 'docs/LICENSE.txt';
 $themeversion['xhtml'] = true;
