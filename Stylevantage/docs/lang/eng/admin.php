e875f60c05eb3dc3b7c2404a9faa3eb71e8af1b6
TakeIT2 <i.takeit2@gmail.com>
2010-07-30 23:07:14 +0000

    removed

--- Stylevantage/docs/lang/eng/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('_STYLEVANTAGE_COLORSCHEME', 'Color scheme');
-define('_STYLEVANTAGE_ORANGE', 'Orange');
-define('_STYLEVANTAGE_GREEN', 'Green');
-define('_STYLEVANTAGE_BLUE', 'Blue');
-
-?>
--- Stylevantage/docs/lang/eng/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('_STYLEVANTAGE_THEMENAME', 'Stylevantage');
-define('_STYLEVANTAGE_DISPLAYNAME', 'Stylevantage');
-define('_STYLEVANTAGE_DESCRIPTION', 'Stylevantage template');
-
-?>
