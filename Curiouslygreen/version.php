8a93d4f3a2370c2cceff17dd09781cc6985dd959
TakeIT2 <i.takeit2@gmail.com>
2011-01-28 19:47:27 +0000

    removed '_' from the display name

--- Curiouslygreen/version.php
+++ Curiouslygreen/version.php
@@ -1,7 +1,7 @@
 <?php 
 $themeversion['name'] = 'Curiouslygreen';
 $domain = ZLanguage::getThemeDomain($themeversion['name']);
-$themeversion['displayname'] = __('Curiously_Green', $domain);
+$themeversion['displayname'] = __('Curiously Green', $domain);
 $themeversion['description'] = __('Curiously Green OWD template', $domain);
 $themeversion['version'] = '2.1';
 $themeversion['contact'] = 'http://code.zikula.org/themes/, http://www.nodethirtythree.com/';
